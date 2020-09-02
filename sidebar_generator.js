b0f8f7d5b6bb08005715f8cd2906e6b0d64493b8
wangrollin <wangrollin@foxmail.com>
2020-09-02 11:08:49 +0800

    js版本sidebar generator最终版本出炉

--- sidebar_generator.js
+++ sidebar_generator.js
@@ -6,34 +6,32 @@ let prefix = '* '
 
 function write_to_sidebar(path, indent, sideBarFile) {
 
-    fs.readdir(path, (err, files) => {
+    let files = fs.readdirSync(path);
+    files.sort(((fileName1, fileName2) => fileName1 < fileName2 ? -1 : 1));
 
-        files.sort(((fileName1, fileName2) => fileName1 < fileName2 ? -1 : 1));
+    files.forEach(file => {
 
-        files.forEach(file => {
+        if (!fs.statSync(path + '/' + file).isDirectory() && file.includes('.DS_Store')
+            || !fs.statSync(path + '/' + file).isDirectory() && file === '_sidebar.md'
+            || fs.statSync(path + '/' + file).isDirectory() && file.includes('.assets')) {
+            return;
+        }
 
-            if (!fs.statSync(path + '/' + file).isDirectory() && file.includes('.DS_Store')
-                || !fs.statSync(path + '/' + file).isDirectory() && file === '_sidebar.md'
-                || fs.statSync(path + '/' + file).isDirectory() && file.includes('.assets')) {
-                return;
+        if (fs.statSync(path + '/' + file).isDirectory()) {
+            let dirName = file;
+            if (dirName.match(/^\d+_\.*/g)) {
+                dirName = dirName.substr(dirName.indexOf('_') + 1, dirName.length)
             }
-
-            if (fs.statSync(path + '/' + file).isDirectory()) {
-                let dirName = file;
-                if (dirName.match(/\d*_.*!/g)) {
-                    dirName = dirName.substr(dirName.indexOf('_') + 1, dirName.length)
-                }
-                fs.appendFileSync(sideBarFile, indent + prefix + '🗂' + dirName + '\n');
-                write_to_sidebar(path + '/' + file, indent + '    ', sideBarFile);
-            } else {
-                file = file.replace('.md', '');
-                if (file.match(/\d*_.*!/g)) {
-                    file = file.substr(file.indexOf('_') + 1, file.length)
-                }
-                fs.appendFileSync(sideBarFile, indent + prefix + '[📝' + file + ']('
-                    + (path + '/' + file).split('github.io')[1] + ')\n');
+            fs.appendFileSync(sideBarFile, indent + prefix + '🗂' + dirName + '\n');
+            write_to_sidebar(path + '/' + file, indent + '    ', sideBarFile);
+        } else {
+            let name_to_show = file.replace('.md', '');
+            if (name_to_show.match(/^\d+_\.*/g)) {
+                name_to_show = name_to_show.substr(file.indexOf('_') + 1, file.length)
             }
-        });
+            fs.appendFileSync(sideBarFile, indent + prefix + '[📝' + name_to_show + ']('
+                + (path + '/' + file).split('github.io')[1] + ')\n');
+        }
     });
 }
 
@@ -46,10 +44,10 @@ function generateSideBar() {
         basePath + '/docs/BigDemo项目文档']
 
     pathList.forEach(path => {
-        fs.open(path + '/_sidebar1.md',
+        fs.open(path + '/_sidebar.md',
             'w',
             '777',
-            (err, fd) => write_to_sidebar(path, '', path + '/_sidebar1.md'));
+            (err, fd) => write_to_sidebar(path, '', path + '/_sidebar.md'));
     });
 }
 
